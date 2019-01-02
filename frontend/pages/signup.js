cb7a02b4f511723e56840ba299842065f6cce7ea
Gray Smith <gray.smith00@gmail.com>
2019-01-01 21:17:54 -0700

    Create initial Signup component and signup page

---
+++ frontend/pages/signup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Signup from '../components/Signup';
+
+const signupPage = props => {
+  return (
+    <div>
+      <h1>signupPage</h1>
+      <Signup />
+    </div>
+  );
+};
+
+export default signupPage;
