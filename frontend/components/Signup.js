c8cb364a259783db8cd4e4f95e933de92e70627d
Gray Smith <gray.smith00@gmail.com>
2019-01-01 21:27:40 -0700

    Build out basic signup form

--- frontend/components/Signup.js
+++ frontend/components/Signup.js
@@ -5,11 +5,52 @@ import Form from './styles/Form';
 import Error from './ErrorMessage';
 
 class Signup extends Component {
+  state = {
+    name: '',
+    email: '',
+    password: ''
+  };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
     return (
       <Form>
         <fieldset>
           <h2>Sign up for a new account!</h2>
+          <label htmlFor="email">
+            Email
+            <input
+              type="email"
+              name="email"
+              placeholder="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label htmlFor="name">
+            Name
+            <input
+              type="text"
+              name="name"
+              placeholder="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label htmlFor="password">
+            Password
+            <input
+              type="password"
+              name="password"
+              placeholder="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+          </label>
+          <button type="submit">Sign Up!</button>
         </fieldset>
       </Form>
     );
--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Signup from '../components/Signup';
+import styled from 'styled-components';
+
+const Columns = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 20px;
+`;
 
 const signupPage = props => {
   return (
-    <div>
-      <h1>signupPage</h1>
+    <Columns>
       <Signup />
-    </div>
+      <Signup />
+      <Signup />
+    </Columns>
   );
 };
 
