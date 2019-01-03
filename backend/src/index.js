f4baa79313a1e303f867b9b85eb69cf79423eea5
Gray Smith <gray.smith00@gmail.com>
2019-01-02 22:07:52 -0700

    Handle JWT verification with custom express middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 // let's go!
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
@@ -8,7 +9,17 @@ const server = createServer();
 
 // TODO use express middleware to handle cookies (JWT)
 server.express.use(cookieParser());
-// TODO use express middleware to populate current user
+
+// decode the JWT so we can get the user Id on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put userId on req for future requests to access
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
